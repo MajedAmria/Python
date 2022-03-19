9ab4da03b1616b7c4741d7f9bba1b7a06620f769
Majed Amria <majedamrya@hotmail.com>
2022-03-19 14:58:33 +0200

    Python

---
+++ fundemental/helloworld.py
@@ -0,0 +1,12 @@
+print("hello world")
+name="Majed"
+print("Hello",name)
+print("Hello "+name)
+number=25
+print("Hello",number)
+print("Hello "+str(number))
+
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print( "I love to eat {} and {}" .format(fave_food1, fave_food2))
+print( f"I love to eat {fave_food1} and {fave_food2}") 
