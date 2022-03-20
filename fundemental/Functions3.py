507b85c2fe71bdfac7586d1fcbc09512a1dcb400
Majed Amria <majedamrya@hotmail.com>
2022-03-20 21:30:45 +0200

    add condition

--- fundemental/Functions3.py
+++ fundemental/Functions3.py
@@ -1,10 +1,14 @@
 import random
 
 def randInt(min=0, max=100):
-    num = random.randrange(min,max) 
-    return num
+    if max>min and max>0:
+        num = random.randrange(min,max) 
+        return num
+    else:
+        print("can't do anything......!")
+    
 print(randInt()) 		    # should print a random integer between 0 to 100
 print(randInt(max=50)) 	    # should print a random integer between 0 to 50
 print(randInt(min=50)) 	    # should print a random integer between 50 to 100
 print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
-
+print(randInt(max=0))
