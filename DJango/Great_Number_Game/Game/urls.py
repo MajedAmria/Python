804548d5de70d617b98bd5ae1d598618fa9e6b1e
Majed Amria <majedamrya@hotmail.com>
2022-03-30 00:07:51 +0300

    great number

--- DJango/Great_Number_Game/Game/urls.py
+++ DJango/Great_Number_Game/Game/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.show),
+    path('guess',views.guess),
+    path('destroy',views.destroy),
 ]
--- DJango/Great_Number_Game/Game/views.py
+++ DJango/Great_Number_Game/Game/views.py
@@ -1,4 +1,32 @@
-from django.shortcuts import render
+import random 	
+from django.shortcuts import render,redirect
+
 
 def show(request):
+    if  'random' not in request.session:
+        request.session['random'] = random.randint(1, 100)
+        request.session['status'] = 'new'
+        request.session['guesses'] = 0
+    print('Status This Session:', request.session['status'])
+    print('Random Number This Session:', request.session['random'])
     return render(request,"index.html")
+
+
+def guess(request):
+    if request.POST['guess-value']:
+        if int(request.POST['guess-value']) > request.session['random']:
+            request.session['status'] ='high'
+        elif int(request.POST['guess-value']) < request.session['random']:
+            request.session['status'] = 'low'
+        else:
+            request.session['status'] = 'win'
+        request.session['guesses'] += 1
+        if request.session['status'] != 'win' and request.session['guesses'] >= 5:
+            request.session['status'] = 'lose'
+    return redirect("/")
+
+
+def destroy(request):
+    request.session.clear()
+    print('Session Cleared')
+    return redirect("/")
